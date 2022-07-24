80d08cc40b475c0f38bd2b7ad86599df8ccfb050
Valeria Fadeeva <valeria.fadeeva.me@gmail.com>
2022-07-25 02:40:19 +0500

    Added shadows for fonts by percent of font size

--- make_fonts.py
+++ make_fonts.py
@@ -20,6 +20,10 @@ with open('alphabet.txt') as f:
         for font_size in range(14, 50, 2):
             percent = 0.15
             real_font_size = math.floor(font_size - (font_size * percent))
+
+            shadow_x = math.ceil(font_size * 0.0625)
+            shadow_y = math.ceil(font_size * 0.1041)
+
             font = ImageFont.truetype(font_path[0], real_font_size)
 
             cell_width = int((font_size * 6 + 5) / 10)
@@ -37,12 +41,10 @@ with open('alphabet.txt') as f:
 
                 img_char = Image.new("RGBA", (cell_width, cell_height))
 
-                for i, j in itertools.product((-1, 0, -1), (-1, 0, 1)):
-                    ImageDraw.Draw(img_char).text(
-                        (0 + i, 0 + j), str(char), font=font, fill="gray")
+                for i, j in itertools.product((-shadow_x, 0, shadow_x), (-shadow_y, 0, shadow_y)):
+                    ImageDraw.Draw(img_char).text((0 + i, 0 + j), str(char), font=font, fill="gray")
 
-                ImageDraw.Draw(img_char).text(
-                    (0, 0), str(char), font=font, fill=font_color)
+                ImageDraw.Draw(img_char).text((0, 0), str(char), font=font, fill=font_color)
 
                 background.paste(img_char, offset)
 
