50a7c2f4c5e3a8589e44a8e944ea9f7afaafd09b
Valeria Fadeeva <valeria.fadeeva.me@gmail.com>
2022-07-25 23:37:00 +0500

    Fix font shadows

--- make_fonts.py
+++ make_fonts.py
@@ -23,8 +23,8 @@ with open('alphabet.txt') as f:
             percent = 0.15
             real_font_size = math.floor(font_size - (font_size * percent))
 
-            shadow_x = math.ceil(font_size * 0.0625)
-            shadow_y = math.ceil(font_size * 0.1041)
+            shadow_x = math.ceil(font_size * 0.015625)
+            shadow_y = math.ceil(font_size * 0.015625)
 
             font = ImageFont.truetype(font_path[0], real_font_size)
 
@@ -44,16 +44,9 @@ with open('alphabet.txt') as f:
 
                 img_char = Image.new("RGBA", (cell_width, cell_height))
 
-                for i, j in itertools.product((-shadow_x, 0, shadow_x), (-shadow_y, 0, shadow_y)):
-                    ImageDraw.Draw(img_char).text((0 + i, 0 + j), str(char), font=font, fill=shadow_color)
+                ImageDraw.Draw(img_char).text((0 + shadow_x, 0 + shadow_y), str(char), font=font, fill=shadow_color)
 
                 img_char = img_char.filter(ImageFilter.BLUR)
-                '''
-                n = 0
-                while n < 3:
-                    img_char = img_char.filter(ImageFilter.BLUR)
-                    n += 1
-                '''
 
                 ImageDraw.Draw(img_char).text((0, 0), str(char), font=font, fill=font_color)
 
